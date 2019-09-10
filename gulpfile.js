69630028b276f9d6264bca839d5ba09a96802dd5
upeksha1996 <19upeksha96@gmail.com>
2019-09-10 17:34:38 +0530

    login page user display

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,16 @@
-const gulp = require('gulp');
+//const gulp = require('gulp');
+
+
+const { src,dest,watch,series,parallel } = require('gulp');
+
 const imagemin = require('gulp-imagemin');
 const uglify = require('gulp-uglify');
 const sass = require('gulp-sass');
 const concat = require('gulp-concat');
 const ext_replace = require('gulp-ext-replace');
+
+
+
 
 
 /*
@@ -15,40 +22,55 @@ gulp.watch - Watch files and folders for changes
 */
 
 //Minify js
-gulp.task('minify', function(done) {
-  gulp.src('assets/js/*.js')
+function minify(done) {
+  src('assets/js/*.js')
     .pipe(uglify())
-    .pipe(gulp.dest('public/javascripts'));
+    .pipe(dest('public/javascripts'));
   done();
-});
+};
+
 //Optimizr images
-gulp.task('imageMin', function(done) {
-  gulp.src('assets/images/*')
+function imageMin(done) {
+  src('assets/images/*')
     .pipe(imagemin())
-    .pipe(gulp.dest('public/images'));
+    .pipe(dest('public/images'));
   done();
-});
+};
 //Compile sass
-gulp.task('sass', function(done) {
-  gulp.src('assets/sass/*.scss')
+function scss(done) {
+  src('assets/sass/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('public/stylesheets'));
+    .pipe(dest('public/stylesheets'));
   done();
-});
+};
 //Scripts
-gulp.task('scripts', function(done) {
-  gulp.src('assets/js/*.js')
+function scripts(done) {
+  src('assets/js/*.js')
     .pipe(uglify())
-    .pipe(gulp.dest('public/javascripts'));
+    .pipe(dest('public/javascripts'));
   done();
-});
+};
+
+
+
+function watchTask(){
+  watch('assets/js/*.js', scripts);
+  watch('assets/images/*', imageMin);
+  watch('assets/sass/*.sass', scss);
+  //watch('assets/*.html', copyHtml);
+}
+
+exports.default = series(
+  parallel(
+    scss,
+    scripts,
+    imageMin
+  ),
+  minify,
+  watchTask
+);
+
 // gulp.task('default', gulp.series('imageMin', 'sass', 'scripts', function(done) {
 //   // default task code here
 //   done();
 // }));
-gulp.task('watch', function() {
-  gulp.watch('assets/js/*.js', gulp.series('scripts'));
-  gulp.watch('assets/images/*', gulp.series('imageMin'));
-  gulp.watch('assets/sass/*.sass', gulp.series('sass'));
-  gulp.watch('assets/*.html', gulp.series('copyHtml'));
-});
